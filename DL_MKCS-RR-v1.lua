31875e2ea2edd02c5a352efdb98a1006001d8793
Judas Drekonym <koopaemails@gmail.com>
2025-05-24 20:17:59 -0400

    Current addon set

---
+++ DL_MKCS-RR-v1.lua
@@ -0,0 +1,178 @@
+freeslot(
+	"SKINCOLOR_LEMONGRASS",
+	"SKINCOLOR_LAWN",
+	"SKINCOLOR_TOURMALINE",
+	"SKINCOLOR_SOAP",
+	"SKINCOLOR_FROST",
+	"SKINCOLOR_GLASS",
+	"SKINCOLOR_SLEEPY",
+	"SKINCOLOR_GAG",
+	"SKINCOLOR_FOLIAGE",
+	"SKINCOLOR_DAYBREAK",
+	"SKINCOLOR_COSTAL",
+	"SKINCOLOR_WISTERIA",
+	"SKINCOLOR_CORAL",
+	"SKINCOLOR_JUNGLE",
+	"SKINCOLOR_BRASS",
+	"SKINCOLOR_ELECTRUM"
+	)
+
+skincolors[SKINCOLOR_SLEEPY] = {
+	name = "Sleepy",
+	ramp = {122, 122, 122, 123, 141, 135, 136, 136, 172, 172, 167, 167, 168, 168, 169, 187},
+	invcolor = SKINCOLOR_MINT,
+	invshade = 9,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_SAPPHIRE, SKINCOLOR_ULTRAMARINE)
+
+skincolors[SKINCOLOR_SOAP] = {
+	name = "Soap",
+	ramp = {0, 128, 120, 121, 121, 122, 123, 123, 141, 141, 135, 136, 137, 138, 139, 254},
+	invcolor = SKINCOLOR_MAGENTA,
+	invshade = 9,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_AQUAMARINE, SKINCOLOR_TURQUOISE)
+
+skincolors[SKINCOLOR_LEMONGRASS] = {
+	name = "Lemon-Grass",
+	ramp = {0, 1, 80, 88, 88, 89, 89, 90, 91, 190, 191, 78, 79, 237, 239, 110},
+	invcolor = SKINCOLOR_BLOSSOM,
+	invshade = 9,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_TEA, SKINCOLOR_PISTACHIO)
+
+skincolors[SKINCOLOR_FOLIAGE] = {
+	name = "Foliage",
+	ramp = {120, 121, 122, 124, 124, 125, 126, 118, 119, 119, 111, 111, 31, 31, 31, 31},
+	invcolor = SKINCOLOR_PURPLE,
+	invshade = 9,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_TURTLE, SKINCOLOR_SWAMP)
+
+skincolors[SKINCOLOR_COSTAL] = {
+	name = "Coastal",
+	ramp = {0, 48, 50, 220, 90, 140, 141, 134, 135, 135, 136, 137, 137, 138, 139, 254},
+	invcolor = SKINCOLOR_YELLOW,
+	invshade = 9,
+	chatcolor = V_SKYMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_ROBIN, SKINCOLOR_CYAN)
+
+skincolors[SKINCOLOR_WISTERIA] = {
+	name = "Wisteria",
+	ramp = {0, 1, 144, 160, 160, 192, 193, 195, 185, 186, 186, 187, 187, 28, 30, 31},
+	invcolor = SKINCOLOR_TEAL,
+	invshade = 9,
+	chatcolor = V_PINKMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_FUCHSIA, SKINCOLOR_TOXIC)
+
+skincolors[SKINCOLOR_BRASS] = {
+	name = "Brass",
+	ramp = {0, 208, 216, 221, 67, 68, 60, 60, 61, 42, 43, 44, 71, 47, 30, 31},
+	invcolor = SKINCOLOR_DUSK,
+	invshade = 9,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_BEIGE, SKINCOLOR_CARAMEL)
+
+skincolors[SKINCOLOR_FROST] = {
+	name = "Frost",
+	ramp = {0, 1, 128, 120, 129, 121, 140, 141, 141, 142, 19, 21, 24, 26, 28, 31},
+	invcolor = SKINCOLOR_TAFFY,
+	invshade = 9,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_TEAL, SKINCOLOR_ROBIN)
+
+skincolors[SKINCOLOR_LAWN] = {
+	name = "Lawn",
+	ramp = {0, 128, 120, 121, 121, 98, 100, 102, 115, 117, 118, 119, 111, 111, 111, 31},
+	invcolor = SKINCOLOR_TAFFY,
+	invshade = 9,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_TURTLE, SKINCOLOR_SWAMP)
+	
+	skincolors[SKINCOLOR_TOURMALINE] = {
+	name = "Tourmaline",
+	ramp = {128, 120, 120, 121, 122, 124, 125, 103, 94, 70, 70, 237, 239, 47, 30, 31},
+	invcolor = SKINCOLOR_DUSK,
+	invshade = 9,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_EMERALD, SKINCOLOR_ALGAE)
+	
+	skincolors[SKINCOLOR_DAYBREAK] = {
+	name = "Daybreak",
+	ramp = {176, 176, 160, 160, 147, 148, 135, 135, 136, 137, 137, 157, 158, 159, 253, 254},
+	invcolor = SKINCOLOR_OLIVE,
+	invshade = 12,
+	chatcolor = V_BLUEMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_BLUEBERRY, SKINCOLOR_THISTLE)
+
+	skincolors[SKINCOLOR_CORAL] = {
+	name = "Coral",
+	ramp = {176, 200, 201, 202, 203, 204, 205, 39, 40, 42, 44, 45, 46, 47, 30, 31},
+	invcolor = SKINCOLOR_SAPPHIRE,
+	invshade = 12,
+	chatcolor = V_PINKMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_POMEGRANATE, SKINCOLOR_LILAC)
+
+	skincolors[SKINCOLOR_GLASS] = {
+	name = "Glass",
+	ramp = {0 ,120, 121, 122 ,141 ,141, 14 ,15, 17, 19, 21, 23, 25, 27, 29, 31},
+	invcolor = SKINCOLOR_BLOSSOM,
+	invshade = 8,
+	chatcolor = V_AQUAMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_SEPIA, SKINCOLOR_PIGEON)
+	
+	skincolors[SKINCOLOR_JUNGLE] = {
+	name = "Jungle",
+	ramp = {80, 49, 50 ,52, 53, 55, 67, 228, 231, 94, 127, 108, 109, 110, 29, 31},
+	invcolor = SKINCOLOR_RASPBERRY,
+	invshade = 11,
+	chatcolor = V_ORANGEMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_VOMIT, SKINCOLOR_CROCODILE)
+	
+skincolors[SKINCOLOR_GAG] = {
+	name = "Gag",
+	ramp = {0, 120, 96, 97, 99, 123, 141, 141, 171, 149, 166, 166, 167, 168, 169, 253},
+	invcolor = SKINCOLOR_BLOSSOM,
+	invshade = 11,
+	chatcolor = V_GREENMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_SAPPHIRE, SKINCOLOR_ULTRAMARINE)
+	
+	skincolors[SKINCOLOR_ELECTRUM] = {
+	name = "Electrum",
+	ramp = {0, 2, 208, 48, 85, 87, 77, 78, 78, 79, 79, 238, 239, 110, 111, 31},
+	invcolor = SKINCOLOR_BLOSSOM,
+	invshade = 11,
+	chatcolor = V_YELLOWMAP,
+	accessible = true
+}
+	//M_MoveColorBefore(SKINCOLOR_YELLOW, SKINCOLOR_COPPER)
